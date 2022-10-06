f9fe022a21a58c6b6e380837e388b66f7962e16f
Taehyun Kim <persona7548@gmail.com>
2022-10-06 18:27:42 +0900

    초기 실행 세팅

--- steamCrawler.py
+++ steamCrawler.py
@@ -5,7 +5,7 @@ import time
 from datetime import datetime
 from fake_useragent import UserAgent
 def saveUserData(steamID,userCount):
-    savef = open("C:/Users/KTH/Desktop/read",'w')
+    savef = open("read.txt",'w')
     print(str(steamID))
     savef.write(str(steamID)+','+str(userCount)+'\n')
     savef.close()
@@ -25,7 +25,7 @@ def print_steam_crawl(steamID,ID_Range,fileNum):
                 steamID = steamID-1
                 saveUserData(steamID,userCount)
                 now = datetime.now()
-                writeLog = open("C:/Users/KTH/Desktop/log.txt",'a')
+                writeLog = open("log.txt",'a')
                 writeLog.write('Error ID: '+str(steamID)+' Time: '+str(now)+'\n')
                 writeLog.close()
                 print("429 Error")
@@ -40,7 +40,7 @@ def print_steam_crawl(steamID,ID_Range,fileNum):
         if (str(SteamGame) != "[]" and 'hours_forever' in gameList):  #Private library Check
             userCount = userCount+1
             saveUserData(steamID,userCount)
-            f = open('C:/Users/KTH/Desktop/'+str(userCount)+'.csv', 'w')
+            f = open(str(userCount)+'.csv', 'w')
             f.write('GameName,Id:'+ string_ID+'\n')
             for course in SteamGame:
                 try: #empty playtime Check
@@ -55,17 +55,26 @@ def print_steam_crawl(steamID,ID_Range,fileNum):
         else:
             saveUserData(steamID,userCount)
             continue
+        
+#초기 실행 이후 주석 해제
+#f = open("read.txt",'r')
+#data = f.read()
+#f.close()
+#saveData = data.split(',')
+#startID = int(saveData[0])
+#fileSave = int(saveData[1])
+#
 
-f = open("C:/Users/KTH/Desktop/read",'r')
-data = f.read()
-f.close()
-saveData = data.split(',')
-startID = int(saveData[0])
-fileSave = int(saveData[1])
+#초기 실행 이후 주석 처리
+startID = 76561197960265728
+fileSave = 0
+#
+
+
 pages = 100000
 
 now = datetime.now()
-writeLog =  open("C:/Users/KTH/Desktop/log.txt",'a')
+writeLog =  open("log.txt",'a')
 writeLog.write('Start ID: '+str(startID)+' Time: '+str(now)+'\n')
 writeLog.close()
 
